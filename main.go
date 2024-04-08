7c5a5f4ca283917a42689fb721944e733b95c380
Oliver Feng <wenlongfeng8@gmail.com>
2024-04-08 15:43:46 -0400

    backend first commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"safebank/internal/repository"
+	"safebank/internal/repository/dao"
+	"safebank/internal/service"
+	"safebank/internal/web"
+	"safebank/internal/web/middleware"
+	"strings"
+	"time"
+)
+
+func main() {
+	db := initDB()
+	server := initWebServer()
+
+	u := initUser(db)
+	u.RegisterRoutes(server)
+
+	server.Run(":8080")
+}
+
+func initWebServer() *gin.Engine {
+	server := gin.Default()
+
+	// CORS (Cross-Origin Resource Sharing)
+	server.Use(cors.New(cors.Config{
+		AllowHeaders:     []string{"Content-Type"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "yourcompany.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	store := cookie.NewStore([]byte("secret"))
+	server.Use(sessions.Sessions("mysession", store))
+	server.Use(middleware.NewLoginMiddlewareBuilder().Build())
+
+	return server
+}
+
+func initUser(db *gorm.DB) *web.UserHandler {
+	ud := dao.NewUserDao(db)
+	repo := repository.NewUserRepository(ud)
+	svc := service.NewUserService(repo)
+	u := web.NewUserHandler(svc)
+	return u
+}
+
+func initDB() *gorm.DB {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(localhost:13318)/safebank"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = dao.InitTable(db)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
