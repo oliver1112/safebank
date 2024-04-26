83d3ea7b6c71ac4401843ca1d2a4676e9f6e6ed3
Wenlong Feng <wenlongfeng@Wenlongs-MacBook-Pro.local>
2024-04-26 17:34:37 -0400

    finish home loan

--- internal/repository/dao/home_loan.go
+++ internal/repository/dao/home_loan.go
@@ -1,6 +1,9 @@
 package dao
 
-import "gorm.io/gorm"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type HomeLoanDAO struct {
 	db *gorm.DB
@@ -24,4 +27,21 @@ func NewHomeLoanDao(db *gorm.DB) *HomeLoanDAO {
 	return &HomeLoanDAO{
 		db: db,
 	}
+}
+
+func (hd *HomeLoanDAO) GetSaving(ctx *gin.Context, userId int64) (HomeLoan, error) {
+	var homeLoan HomeLoan
+	err := hd.db.WithContext(ctx).Where("user_id = ?", userId).First(&homeLoan).Error
+	//err := ud.db.WithContext(ctx).First(&u, "email = ?", email).Error
+	return homeLoan, err
+}
+
+func (hd *HomeLoanDAO) CreateOrUpdate(ctx *gin.Context, data HomeLoan) (HomeLoan, error) {
+	where := HomeLoan{
+		LoanID: data.LoanID,
+	}
+	var homeLoan HomeLoan
+	err := hd.db.Where(where).Assign(data).FirstOrCreate(&homeLoan).Error
+	//err := ud.db.WithContext(ctx).First(&u, "email = ?", email).Error
+	return homeLoan, err
 }
