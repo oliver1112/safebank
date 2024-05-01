2de3c32ca2bd0f51fd5f9318f22ad3f9d7584dc3
Mortihoo <455142722@qq.com>
2024-05-01 17:20:43 -0400

    New features: dashboard. Add Institute table

---
+++ internal/repository/dao/institute.go
@@ -0,0 +1,39 @@
+package dao
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type InstituteDAO struct {
+	Db *gorm.DB
+}
+
+func NewInstituteDao(db *gorm.DB) *InstituteDAO {
+	return &InstituteDAO{
+		Db: db,
+	}
+}
+
+type Institute struct {
+	InstituteID   int64  `gorm:"primaryKey" json:"institute_id"`
+	InstituteName string `gorm:"unique" json:"edu_institute"`
+}
+
+func (i *InstituteDAO) GetByID(ctx *gin.Context, ID int64) (Institute, error) {
+	var institute Institute
+	err := i.Db.WithContext(ctx).Where(&Institute{InstituteID: ID}).First(&institute).Error
+	return institute, err
+}
+
+func (i *InstituteDAO) GetByName(ctx *gin.Context, instituteName string) (Institute, error) {
+	var institute Institute
+	err := i.Db.WithContext(ctx).Where(&Institute{InstituteName: instituteName}).First(&institute).Error
+	return institute, err
+}
+
+func (i *InstituteDAO) CreateOrUpdate(ctx *gin.Context, data Institute) (Institute, error) {
+	var institute Institute
+	err := i.Db.Where(data).Assign(data).FirstOrCreate(&institute).Error
+	return institute, err
+}
